45d1a9be009dd781a177ec979f9f1ec0386fe0f1
Nolwenn Dubourg <johndoe@example.com>
2025-08-02 20:33:24 +0200

    installation Router et mise en place de route et création des pages

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Error from './pages/error/index.jsx'
+import Header from './components/Header/index.jsx'
+import Informations from './pages/informations/index.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Header />
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="*" element={<Error />} />
+                <Route path="/Informations" element={<Informations />} />
+            </Routes>
+    </Router>
+    
   </StrictMode>,
 )
---
+++ my-react-app/src/pages/informations/index.jsx
@@ -0,0 +1,9 @@
+function Informations() {
+    return (
+        <div>
+            <h1>A propos</h1>
+        </div>
+    )
+}
+ 
+export default Informations
